789e66166b4badabace9ad00f33571f76081c041
Jhonatan Raya <jhonatan001314@gmail.com>
2025-12-06 02:56:08 -0400

    dev env configured

--- backend/src/service/chatService.js
+++ backend/src/service/chatService.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 async function sendMessageToRasa(sender, message) {
   try {
     const response = await axios.post(
-      "https://super-funicular-vrgw5r5xq5xf6gqg-5005.app.github.dev/webhooks/rest/webhook",
+      "http://localhost:5005/webhooks/rest/webhook",
       { sender, message }
     );
     return response.data.map(r =>({text: r.text, buttons: r.buttons || []}));
